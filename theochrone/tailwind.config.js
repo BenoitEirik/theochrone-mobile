af721310bb31aa0c0aa54fd2ade6fb1e228469a3
BenoitEirik <olafsergent@gmail.com>
2023-01-10 21:21:01 +0100

    Add Tailwindcss

---
+++ theochrone/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    `./components/**/*.{vue,js,ts}`,
+    `./layouts/**/*.vue`,
+    `./pages/**/*.vue`,
+    `./composables/**/*.{js,ts}`,
+    `./plugins/**/*.{js,ts}`,
+    `./App.{js,ts,vue}`,
+    `./app.{js,ts,vue}`,
+    `./Error.{js,ts,vue}`,
+    `./error.{js,ts,vue}`
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
