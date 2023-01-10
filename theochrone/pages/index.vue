05d27a5c229ef2e8119b0f2ff837cc18abfee0c7
BenoitEirik <olafsergent@gmail.com>
2023-01-10 22:33:23 +0100

    Frame of a fest

---
+++ theochrone/pages/index.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <h1>Mardi de la férie du temps de l'Épiphanie (Temps de Noël)</h1>
+        <div class="flex flex-col">
+            <img src="@/assets/images/image_not_found.png">
+            <table>
+                <tr>
+                    <th>Propre</th>
+                    <td>Romain</td>
+                </tr>
+                <tr>
+                    <th>Édition</th>
+                    <td>1962</td>
+                </tr>
+                <tr>
+                    <th>Célébration</th>
+                    <td>Cette fête peut être célébrée.</td>
+                </tr>
+                <tr>
+                    <th>Classe</th>
+                    <td>4</td>
+                </tr>
+                <tr>
+                    <th>Couleur liturgique</th>
+                    <td>Blanc</td>
+                </tr>
+                <tr>
+                    <th>Temporal</th>
+                    <td>Oui</td>
+                </tr>
+                <tr>
+                    <th>Sanctoral</th>
+                    <td>Non</td>
+                </tr>
+                <tr>
+                    <th>Temps liturgique</th>
+                    <td>Temps de l'Épiphanie (Temps de Noël)</td>
+                </tr>
+                <tr>
+                    <th>Fête transférée</th>
+                    <td>Non</td>
+                </tr>
+            </table>
+            <a href="http://introibo.fr/Messe-du-1er-dimanche-apres-l">Textes de la messe et de l'office sur Introibo.fr</a>
+        </div>
+    </div>
+</template>
