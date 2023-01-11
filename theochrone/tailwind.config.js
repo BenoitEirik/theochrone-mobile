3f533556fbf78026e44e9e5cfc585e3399b5a9ed
BenoitEirik <olafsergent@gmail.com>
2023-01-11 01:44:53 +0100

    Update

--- theochrone/tailwind.config.js
+++ theochrone/tailwind.config.js
@@ -1,31 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-
-// tailwind.config.js
-const colors = require('tailwindcss/colors')
-const indielayer = require('@indielayer/ui/tailwind.preset')
-
 module.exports = {
-  darkMode: 'class',
-  // load indielayer ui presets
-  presets: [indielayer()],
-  // allow PurgeCSS to analyze components
   content: [
     './index.html',
     './**/*.vue',
     './src/**/*.{vue,js,ts,jsx,tsx}',
-    'node_modules/@indielayer/ui/**/*',
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: colors.emerald,
-        secondary: colors.slate,
-        success: colors.green,
-        warning: colors.yellow,
-        error: colors.red,
-        'blue-light': '#55acee'
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
