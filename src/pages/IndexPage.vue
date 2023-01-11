b44ae0ba6f78adfde6319ef71932ccc6a6323141
BenoitEirik <olafsergent@gmail.com>
2023-01-12 00:45:49 +0100

    Add 3D carousel

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,14 +1,39 @@
 <template>
-<q-page class="q-pa-md">
+<q-page>
   <q-date v-model="date" minimal class="full-width no-shadow" />
+  <swiper effect="coverflow" :grabCursor="true" :centeredSlides="true" slidesPerView="auto" :modules="modules"
+    :coverflowEffect="{
+      rotate: 50,
+      stretch: 0,
+      depth: 400,
+      modifier: 1,
+      slideShadows: false,
+    }">
+    <swiper-slide>
+      <img src="~assets/images/image_not_found.png" />
+    </swiper-slide>
+    <swiper-slide>
+      <img src="https://www.saint-louis-des-francais.org/images/articles/saint-martin-de-tours.jpg" />
+    </swiper-slide><swiper-slide>
+      <img src="https://www.introibo.fr/IMG/jpg/1111mennas.jpg" />
+    </swiper-slide>
+  </swiper>
 </q-page>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
 
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { EffectCoverflow, Pagination } from 'swiper';
+import 'swiper/css';
+
 export default defineComponent({
   name: 'IndexPage',
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
   setup() {
     function formatDate(date: Date) {
       var d = new Date(date),
@@ -25,8 +50,24 @@ export default defineComponent({
     }
 
     return {
-      date: ref(formatDate(new Date()))
+      date: ref(formatDate(new Date())),
+      modules: [EffectCoverflow, Pagination],
     }
   }
 });
 </script>
+
+<style>
+.swiper-slide {
+  background-position: center;
+  background-size: cover;
+  width: 70%;
+  height: 300px;
+}
+
+.swiper-slide img {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+}
+</style>
