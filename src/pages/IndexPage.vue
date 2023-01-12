ebf61bf1847afc621efba979aeffaf453c7aa189
BenoitEirik <olafsergent@gmail.com>
2023-01-12 19:49:41 +0100

    Add footer on main page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,14 +1,15 @@
 <template>
-<q-page>
+<q-page class="full-width full-height column no-wrap justify-between items-stretch">
   <q-date v-model="date" minimal class="full-width no-shadow" />
-  <swiper effect="coverflow" :grabCursor="true" :centeredSlides="true" slidesPerView="auto" :modules="modules"
-    :coverflowEffect="{
+
+  <swiper :effect="'coverflow'" :grabCursor="true" :centeredSlides="true" :slidesPerView="'auto'" :modules="modules"
+    :pagination="true" :coverflowEffect="{
       rotate: 50,
       stretch: 0,
       depth: 400,
       modifier: 1,
       slideShadows: false,
-    }">
+    }" class="col-grow">
     <swiper-slide>
       <img src="~assets/images/image_not_found.png" />
     </swiper-slide>
@@ -18,6 +19,23 @@
       <img src="https://www.introibo.fr/IMG/jpg/1111mennas.jpg" />
     </swiper-slide>
   </swiper>
+
+  <div class="q-pa-md">
+    <div class="q-pa-sm full-width row jutify-between items-center no-wrap box-title">
+      <div class="row justify-start items-center full-height" style="width: 60px">
+        <q-img src="~assets/images/ornements/gold.png" class="full-height" fit="contain" />
+      </div>
+
+      <div class="full-height col-grow column justify-center items-center no-wrap">
+        <p class="q-ma-none text-h6">Sancty Policarpi</p>
+        <p class="q-ma-none">Test 2</p>
+      </div>
+
+      <div class="row justify-center items-center full-height" style="width: 60px">
+        <q-icon name="arrow_forward_ios" size="1.6em" />
+      </div>
+    </div>
+  </div>
 </q-page>
 </template>
 
@@ -26,6 +44,7 @@ import { defineComponent, ref } from 'vue';
 
 import { Swiper, SwiperSlide } from 'swiper/vue';
 import { EffectCoverflow, Pagination } from 'swiper';
+import 'swiper/css/pagination';
 import 'swiper/css';
 
 export default defineComponent({
@@ -57,7 +76,7 @@ export default defineComponent({
 });
 </script>
 
-<style>
+<style scoped>
 .swiper-slide {
   background-position: center;
   background-size: cover;
@@ -69,5 +88,11 @@ export default defineComponent({
   width: 100%;
   height: 100%;
   object-fit: contain;
+}
+
+.box-title {
+  height: 75px;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 0px 5px -1px, rgba(0, 0, 0, 0.3) 0px 0px 3px -1px;
+  border-radius: 8px;
 }
 </style>
