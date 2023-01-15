79e1b7a70b7f35e37cf1e8e7cdd7c6d1315f2dfa
BenoitEirik <olafsergent@gmail.com>
2023-01-16 00:33:08 +0100

    Update notification setup

--- src-capacitor/android/app/src/main/java/com/benoiteirik/theochrone/BootNotification.java
+++ src-capacitor/android/app/src/main/java/com/benoiteirik/theochrone/BootNotification.java
@@ -1,9 +1,13 @@
 package com.benoiteirik.theochrone;
 
 import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.util.Log;
 
 import androidx.core.app.NotificationCompat;
@@ -11,20 +15,40 @@ import androidx.core.app.NotificationManagerCompat;
 
 public class BootNotification extends BroadcastReceiver {
 
+  @SuppressLint("UnsafeProtectedBroadcastReceiver")
   @Override
   public void onReceive(Context context, Intent intent) {
-    Log.d("BOOT", "Notification starting...");
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      CharSequence name = context.getString(R.string.app_name);
+      String description = context.getString(R.string.app_name);
+      int importance = NotificationManager.IMPORTANCE_HIGH;
+      NotificationChannel channel = new NotificationChannel("CHANNEL_ID", name, importance);
+      channel.setDescription(description);
+      channel.enableVibration(false);
+      channel.enableLights(false);
+      channel.setVibrationPattern(null);
+      channel.setLockscreenVisibility(NotificationCompat.VISIBILITY_PUBLIC);
+      // Register the channel with the system; you can't change the importance
+      // or other notification behaviors after this
+      NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
+      notificationManager.createNotificationChannel(channel);
+      Log.d("BOOT", "Notification starting...");
+    }
 
-    NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "BootNotification")
-      .setSmallIcon(R.drawable.ic_launcher_background)
-      .setContentTitle("Theochrone")
-      .setContentText("Deuxième Dimanche après l'Épiphanie")
+    NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "CHANNEL_ID")
+      .setSmallIcon(R.mipmap.ic_launcher_foreground)
+      .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher_foreground))
+      .setStyle(new NotificationCompat.BigPictureStyle()
+        .bigLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher_foreground)))
+      .setContentTitle("Dimanche 15 Janvier 2023")
+      .setContentText("• Deuxième Dimanche après l'Épiphanie\n• Saint Paul, premier ermite et Confesseur\n• Saint Maur, abbé")
       .setStyle(new NotificationCompat.BigTextStyle()
-        .bigText("Much longer text that cannot fit one line..."))
-      .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        .bigText("• Deuxième Dimanche après l'Épiphanie\n• Saint Paul, premier ermite et Confesseur\n• Saint Maur, abbé"))
+      .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+      .setVisibility(NotificationCompat.VISIBILITY_PUBLIC);
+
 
     NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
-    notificationManager.notify(NotificationManagerCompat.IMPORTANCE_DEFAULT, builder.build());
-
+    notificationManager.notify(1, builder.build());
   }
 }
--- src-capacitor/android/app/src/main/java/com/benoiteirik/theochrone/MainActivity.java
+++ src-capacitor/android/app/src/main/java/com/benoiteirik/theochrone/MainActivity.java
@@ -1,6 +1,15 @@
 package com.benoiteirik.theochrone;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.getcapacitor.BridgeActivity;
 
@@ -8,6 +17,38 @@ public class MainActivity extends BridgeActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    NotificationHelper notificationHelper = new NotificationHelper(this);
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      CharSequence name = getString(R.string.app_name);
+      String description = getString(R.string.app_name);
+      int importance = NotificationManager.IMPORTANCE_HIGH;
+      NotificationChannel channel = new NotificationChannel("CHANNEL_ID", name, importance);
+      channel.setDescription(description);
+      channel.enableVibration(false);
+      channel.enableLights(false);
+      channel.setVibrationPattern(null);
+      channel.setLockscreenVisibility(NotificationCompat.VISIBILITY_PUBLIC);
+      // Register the channel with the system; you can't change the importance
+      // or other notification behaviors after this
+      NotificationManager notificationManager = getSystemService(NotificationManager.class);
+      notificationManager.createNotificationChannel(channel);
+      Log.d("BOOT", "Notification starting...");
+    }
+
+
+    NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "CHANNEL_ID")
+      .setSmallIcon(R.mipmap.ic_launcher_foreground)
+      .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher_foreground))
+      .setStyle(new NotificationCompat.BigPictureStyle()
+        .bigLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher_foreground)))
+      .setContentTitle("Dimanche 15 Janvier 2023")
+      .setContentText("• Deuxième Dimanche après l'Épiphanie\n• Saint Paul, premier ermite et Confesseur\n• Saint Maur, abbé")
+      .setStyle(new NotificationCompat.BigTextStyle()
+        .bigText("• Deuxième Dimanche après l'Épiphanie\n• Saint Paul, premier ermite et Confesseur\n• Saint Maur, abbé"))
+      .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+      .setVisibility(NotificationCompat.VISIBILITY_PUBLIC);
+
+
+    NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+    notificationManager.notify(1, builder.build());
   }
 }
