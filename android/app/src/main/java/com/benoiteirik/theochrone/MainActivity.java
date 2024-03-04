e8b4368676458e64e50dd13e7f26be9f7002f67f
BenoitEirik <olafsergent@gmail.com>
2024-03-04 22:47:30 +0100

    Add android directory

---
+++ android/app/src/main/java/com/benoiteirik/theochrone/MainActivity.java
@@ -0,0 +1,5 @@
+package com.benoiteirik.theochrone;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   devtools: { enabled: false },
   experimental: {
     // because: https://github.com/nuxt/nuxt/issues/20889
