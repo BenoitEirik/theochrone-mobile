d2b247f497e315a23ef30eff25ac46fe44c37fbe
BenoitEirik <olafsergent@gmail.com>
2024-03-05 14:37:43 +0100

    Add statusbar

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
+<script setup lang="ts">
+import { StatusBar, Style } from '@capacitor/status-bar'
+
+StatusBar.setStyle({ style: Style.Dark })
+StatusBar.setBackgroundColor({ color: '#55acee' })
+</script>
+
 <template>
-  <div class="w-screen max-w-full h-screen max-h-screen overflow-hidden">
+  <div class="w-screen h-screen max-w-full max-h-screen overflow-hidden">
     <NavHeader />
     <NuxtPage />
     <NavDrawer />
