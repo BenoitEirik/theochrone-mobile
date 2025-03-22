c679a8482e15a5f049aed62e2b590f8e77c8e9c4
BenoitEirik <olafsergent@gmail.com>
2025-03-22 17:51:13 +0100

    reafactor: upgrade Android project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ import icons from './icons'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: '2025-03-22',
   ssr: false,
   devtools: { enabled: false },
   experimental: {
